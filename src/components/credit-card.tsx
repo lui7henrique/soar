26f51f3b4e7bfc829cc010c02ea3db9ce7ee1620
Luiz Henrique <7henrique18@gmail.com>
2024-12-20 16:47:15 -0300

    feat: updates CreditCard component styles for better responsiveness

--- src/components/credit-card.tsx
+++ src/components/credit-card.tsx
@@ -10,16 +10,22 @@ export function CreditCard({ variant = "default", ...props }: CreditCardProps) {
 	return (
 		<div
 			className={cn(
-				"rounded-[25px] flex flex-col font-lato w-[350px] h-[235px] flex-shrink-0",
+				"rounded-[25px] flex flex-col font-lato flex-shrink-0 w-[265px] h-[170px]",
+				"desktop:w-[350px] desktop:h-[235px]",
 				variant === "outline" && "bg-background border",
 				variant === "default" &&
 					"bg-[linear-gradient(107.38deg,_#5B5A6F_2.61%,_#000000_101.2%)]",
 			)}
 			{...props}
 		>
-			<div className="pt-[24px] pl-[26px] pr-[24px] pb-[24px] flex-1 relative">
+			<div
+				className={cn(
+					"flex-1 relative px-5 py-4",
+					"desktop:pt-[24px] desktop:pl-[26px] desktop:pr-[24px] desktop:pb-[24px]",
+				)}
+			>
 				<div className="flex justify-between items-center">
-					<div>
+					<div className="flex flex-col gap-0">
 						<span
 							className={cn(
 								"text-xs",
@@ -31,7 +37,7 @@ export function CreditCard({ variant = "default", ...props }: CreditCardProps) {
 						</span>
 						<p
 							className={cn(
-								"text-xl font-semibold",
+								"text-md desktop:text-xl font-semibold",
 								variant === "default" && "text-white",
 								variant === "outline" && "text-muted-foreground",
 							)}
@@ -41,6 +47,7 @@ export function CreditCard({ variant = "default", ...props }: CreditCardProps) {
 					</div>
 					<figure
 						className={cn(
+							"[&>svg]:w-[29px] desktop:[&>svg]:w-auto",
 							variant === "default" && "text-white",
 							variant === "outline" && "text-foreground",
 						)}
@@ -49,7 +56,7 @@ export function CreditCard({ variant = "default", ...props }: CreditCardProps) {
 					</figure>
 				</div>
 
-				<div className="grid grid-cols-2 mt-7">
+				<div className={cn("grid grid-cols-2 mt-3", "desktop:mt-7")}>
 					<div>
 						<span
 							className={cn(
@@ -62,7 +69,7 @@ export function CreditCard({ variant = "default", ...props }: CreditCardProps) {
 						</span>
 						<p
 							className={cn(
-								"font-semibold",
+								"text-xs desktop:text-md font-semibold",
 								variant === "default" && "text-white",
 								variant === "outline" && "text-muted-foreground",
 							)}
@@ -83,7 +90,7 @@ export function CreditCard({ variant = "default", ...props }: CreditCardProps) {
 						</span>
 						<p
 							className={cn(
-								"font-semibold",
+								"text-xs desktop:text-md font-semibold",
 								variant === "default" && "text-white",
 								variant === "outline" && "text-muted-foreground",
 							)}
@@ -103,7 +110,7 @@ export function CreditCard({ variant = "default", ...props }: CreditCardProps) {
 			>
 				<span
 					className={cn(
-						"text-[22px] font-medium",
+						"text-sm desktop:text-[22px] font-medium",
 						variant === "default" && "text-white",
 					)}
 				>
@@ -111,7 +118,7 @@ export function CreditCard({ variant = "default", ...props }: CreditCardProps) {
 				</span>
 				<figure
 					className={cn(
-						"text-[#9199AF80]/50",
+						"[&>svg]:w-[27px] desktop:[&>svg]:w-auto text-[#9199AF80]/50",
 						variant === "default" && "text-white/50",
 					)}
 				>
